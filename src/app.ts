480e69566d7daa992e6b5e6b70536523442d8c33
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2023-11-16 15:34:38 -0300

    chore: add cors

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,11 @@ import "reflect-metadata";
 import express, { Application } from "express";
 import { routes } from "./routes";
 import { handleError } from "./middlewares/error.midleware";
+import cors from "cors";
 
 const app: Application = express();
+
+app.use(cors());
 
 app.use(express.json());
 
